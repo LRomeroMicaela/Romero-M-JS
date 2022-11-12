7afa9e2afff033223b29ff4c976fda6e49c0cf7c
Micaela Romero <rom_micaela@hotmail.com>
2022-11-11 21:16:57 -0300

    rearmando todo

--- carrito.js
+++ carrito.js
@@ -1,107 +1,149 @@
 //saco del localStorage el item seleccionado por el usuario
 let almacenados = JSON.parse(localStorage.getItem("lentes"));
 let seleccionadoIdStock = JSON.parse(localStorage.getItem("stockId"));
-const carrito = [] ;
-console.log (carrito);
-const carritoStockId = [];
-console.log(carritoStockId);
+//const carrito = [] ;
+//console.log (carrito);
+// const carritoStockId = [];
+// console.log(carritoStockId);
 
-extraerProdSeleccinado();
+const carrito = JSON.parse(localStorage.getItem("lentes")) || [];
+const select = document.getElementById("cantidadAComprar");
+const boton = document.getElementById("hacerPedido");
+let divConImg = ``;
 
-//funcion para extraer los item del array almacenados
-function extraerProdSeleccinado() {
-    if (almacenados != null){
-        for (let item of almacenados) {
-            let { cardImg, cardPrecio, cardTitle } = item;
-            console.log(item);
-            console.log(cardImg, cardPrecio, cardTitle);
-            carrito.push({cardTitle, cardPrecio, cardImg});
-            mostrarImgDeProdSeleccionado();
-            extraerDataStock();
-        }
-    }
-}
-//armado seccion donde muestra producto elegido
-function mostrarImgDeProdSeleccionado(){
-    for (item of almacenados){
-    let divConImg = document.createElement("div")
-    divConImg.innerHTML += `
-        <div class="seleccionado">
-            <div class="seleccionado-img col-10"><img width="100%" src=${item.cardImg} alt="producto elegido"></div>
-            <div class="seleccionado-description col-10"><p>Descripción del producto seleccionado: ${item.cardTitle}</p></div>
-            <div class="seleccionado-material col-10"><p>Precio por unidad: $ ${item.cardPrecio}</p></div>
-        </div>
-    `
-    document.getElementById("artElegido").append(divConImg);
-    }
-   multiplicarPorLaCantDeseada();
-}
+const renderizarProductos = () => {
+	const contenedor = document.getElementById("artElegido");
+	contenedor.innerHTML = "";
 
-//funcion para vaciar carrito y localStorage
-//variable para borrar carrito, con close boton
+	carrito.forEach((id) => {
+		const producto = lentesDisponibles.find(
+			(producto) => producto.id === parseInt(id),
+		);
+		divConImg = document.createElement("div")
+			divConImg.innerHTML = `
+				<div class="seleccionado">
+					<div class="seleccionado-img col-10"><img width="100%" src=${producto.img} alt="producto elegido"></div>
+					<div class="seleccionado-description col-10"><p>Descripción del producto seleccionado: ${producto.nombre} , ${producto.marca}</p></div>
+					<div class="seleccionado-material col-10"><p>Precio por unidad: $ ${producto.precio}</p></div>
+				</div>
+			`
+			document.getElementById("artElegido").append(divConImg);
+	});
+};
+
+renderizarProductos();
+
+const cantidadAComprar = document.getElementById(`cantidadAComprar`)
+cantidadAComprar.addEventListener("click", (e) => {
+	e.preventDefault();
+	const valor = parseInt(select.options[select.selectedIndex].value);
+	const productosGuardados = JSON.parse(localStorage.getItem("lentesDisponibles"));
+	carrito.forEach((id) => {
+		const armazon = productosGuardados.find(
+			(armazon) => armazon.id === parseInt(id),
+		);
+		armazon.stock = armazon.stock - valor;
+		console.log[armazon.stock];
+	});
+	localStorage.setItem("lentesDisponibles", JSON.stringify(productosGuardados));
+	console.log(productosGuardados);
+});
 
 const clearCarrito = document.getElementById(`boton-close`);
     clearCarrito.addEventListener(`click`, clearHTML);
 function clearHTML(){
-    const eliminarloTodo = localStorage.removeItem(`lentes`,`stockId`, `stockActualizado`);
-    artElegido.innerHTML = "";
-    almacenados = "";    
+    const eliminarlentes = localStorage.removeItem(`lentes`);
+    divConImg.innerHTML = ``; 
+	carrito = []; 
 }
 
-function multiplicarPorLaCantDeseada(){
-    for (let armazon of carrito){
-    let { cardPrecio } = armazon;
-    console.log (cardPrecio);
-    const select = document.getElementById("cantidadAComprar");
-    select.addEventListener("change",()=>{
-        const valor = parseInt(select.options[select.selectedIndex].value);
-        if (valor === 1){
-            alert (`El valor a abonar es de $ ` + cardPrecio);
-        } else if(valor === 2){
-            let priceTotal = cardPrecio * 2;
-            alert (`El valor a abonar es de $ `+ priceTotal);
-        } else if (valor === 3){
-            let priceTotal2 = cardPrecio * parseInt(3);
-            alert (`El valor a abonar es de $ ` + priceTotal2);
-        }
-        enviarAIndexActualizStock(valor);
-        //restarAlStock(valor);
-    });
-    }
-    
-}
-
-//funcion para extraer stock y id del producto seleccionado, que se guarda en carritoStockId
-function extraerDataStock() {
-    if (seleccionadoIdStock != null){
-        for (let item of seleccionadoIdStock) {
-            let { idProd, stockDispo } = item;
-            console.log(item);
-            console.log(idProd, stockDispo);
-            carritoStockId.push({idProd, stockDispo});
-        }
-    }
-};
-
-// function restarAlStock(value){
-//         if(value != undefined || null ){
-//         for (let stock of carritoStockId){
-//                 let {stockDispo} = stock
-//                     //let restarAlStock = stockDispo - value; 
-//                     enviarAIndexActualizStock(restarAlStock);  
-//         }        
+//funcion para extraer los item del array almacenados
+// function extraerProdSeleccinado() {
+//     if (almacenados != null){
+//         for (let item of almacenados) {
+//             let { cardImg, cardPrecio, cardTitle } = item;
+//             console.log(item);
+//             console.log(cardImg, cardPrecio, cardTitle);
+//             carrito.push({cardTitle, cardPrecio, cardImg});
+//             mostrarImgDeProdSeleccionado();
+//             extraerDataStock();
+//         }
 //     }
 // }
+// //armado seccion donde muestra producto elegido
+// function mostrarImgDeProdSeleccionado(){
+//     for (item of almacenados){
+//     let divConImg = document.createElement("div")
+//     divConImg.innerHTML += `
+//         <div class="seleccionado">
+//             <div class="seleccionado-img col-10"><img width="100%" src=${item.cardImg} alt="producto elegido"></div>
+//             <div class="seleccionado-description col-10"><p>Descripción del producto seleccionado: ${item.cardTitle}</p></div>
+//             <div class="seleccionado-material col-10"><p>Precio por unidad: $ ${item.cardPrecio}</p></div>
+//         </div>
+//     `
+//     document.getElementById("artElegido").append(divConImg);
+//     }
+//    multiplicarPorLaCantDeseada();
+// }
 
-function enviarAIndexActualizStock (value){
-    if (value != null || undefined){
-        carritoStockId.push({value});
-        localStorage.setItem(`stockActualizado`, JSON.stringify(carritoStockId));
-    }
+// //funcion para vaciar carrito y localStorage
+// //variable para borrar carrito, con close boton
 
-}
 
-//NOTAS PARA MI
-//devolver el stock al index actualizarlo y borrar el localStorage total 
-// validar input
-//crear un alert con los datos del comprador y lo elegido.
+// function multiplicarPorLaCantDeseada(){
+//     for (let armazon of carrito){
+//     let { cardPrecio } = armazon;
+//     console.log (cardPrecio);
+//     const select = document.getElementById("cantidadAComprar");
+//     select.addEventListener("change",()=>{
+//         const valor = parseInt(select.options[select.selectedIndex].value);
+//         if (valor === 1){
+//             alert (`El valor a abonar es de $ ` + cardPrecio);
+//         } else if(valor === 2){
+//             let priceTotal = cardPrecio * 2;
+//             alert (`El valor a abonar es de $ `+ priceTotal);
+//         } else if (valor === 3){
+//             let priceTotal2 = cardPrecio * parseInt(3);
+//             alert (`El valor a abonar es de $ ` + priceTotal2);
+//         }
+//         enviarAIndexActualizStock(valor);
+//         //restarAlStock(valor);
+//     });
+//     }
+    
+// }
+
+// //funcion para extraer stock y id del producto seleccionado, que se guarda en carritoStockId
+// function extraerDataStock() {
+//     if (seleccionadoIdStock != null){
+//         for (let item of seleccionadoIdStock) {
+//             let { idProd, stockDispo } = item;
+//             console.log(item);
+//             console.log(idProd, stockDispo);
+//             carritoStockId.push({idProd, stockDispo});
+//         }
+//     }
+// };
+
+// // function restarAlStock(value){
+// //         if(value != undefined || null ){
+// //         for (let stock of carritoStockId){
+// //                 let {stockDispo} = stock
+// //                     //let restarAlStock = stockDispo - value; 
+// //                     enviarAIndexActualizStock(restarAlStock);  
+// //         }        
+// //     }
+// // }
+
+// function enviarAIndexActualizStock (value){
+//     if (value != null || undefined){
+//         carritoStockId.push({value});
+//         localStorage.setItem(`stockActualizado`, JSON.stringify(carritoStockId));
+//     }
+
+// }
+
+// //NOTAS PARA MI
+// //devolver el stock al index actualizarlo y borrar el localStorage total 
+// // validar input
+// //crear un alert con los datos del comprador y lo elegido.
--- index.js
+++ index.js
@@ -1,35 +1,16 @@
-//construyo una clase creadora de objetos
-class Lentes{
-    constructor(id, img, nombre, marca, modelo, color, precio, stock){
-        this.id = id;
-        this.img = img;
-        this.nombre = nombre;
-        this.marca = marca;
-        this.modelo = modelo;
-        this.color = color;
-        this.precio = precio;
-        this.stock = stock;
-    }
-}
-
-//creacion de objetos de la clase Lentes
-const armazon1 = new Lentes(100,"./img/aviadorBless.jpg","Armazón", "Bless", "Aviador", "Negro", 7500, 10);
-const armazon2 = new Lentes(101, "./img/davorHSol.jpg", "Lentes de Sol", "Davor", "Envolvente", "Negro", 7500, 100);
-const armazon3 = new Lentes(102, "./img/hexagonalBless.jpg", "Armazón", "Bless", "Hexagonal", "Dos colores", 6500, 10);
-const armazon4 = new Lentes(103, "./img/pinUp.jpg", "Armazón", "Bless", "Pin Up", "Dorado y Negro", 9000, 10);
-const armazon5 = new Lentes(104, "./img/solMildura.jpg", "Lentes de Sol","Mildura", "Pin Up", "Dorado y Rosa", 10500, 10);
-const armazon6 = new Lentes(105, "./img/solMuzik.jpg", "Lentes de Sol", "Muzik", "Wayfarer", "Negro", 8500, 10);
-
-let lentesDisponibles = [armazon1, armazon2, armazon3, armazon4, armazon5, armazon6];
-
 //array que corresponde al articulo seleccionado por el cliente.
 const arrayProductos = [];
+console.log(arrayProductos);
+lentesDisponibles = JSON.parse(localStorage.getItem("lentesDisponibles")) || [];
+console.log (lentesDisponibles);
+
+armadoCard();
 
 //funcion para armar las card por JS
-function armadoCard(){
-    for (let armazon of lentesDisponibles){
-        let card = document.createElement("div")
-        card.innerHTML += `
+function armadoCard() {
+	for (let armazon of lentesDisponibles) {
+		let card = document.createElement("div");
+		card.innerHTML += `
             <div class="card-body">
             <img src = ${armazon.img} class="card-img-top" alt = ${armazon.nombre}>
               <h5 class="card-title txt-login">${armazon.nombre}, ${armazon.marca}</h5>
@@ -37,83 +18,90 @@ function armadoCard(){
               <h6 class="txt">$</h6>
               <h6 class="card-txt price">${armazon.precio}</h6>
               </div>
-              <a id="btn-add" href="#" onClick="restarAlStock(${armazon.id}, ${armazon.stock})" class="btn btn-primary subm1">Agregar al carrito</a>
-        </div>`; 
-        document.getElementById("container-productos").append(card); 
-        card.className ="card col-10 col-md-3 img-cat main-img";   
-    }
+              <a id="btn-add" data-id='${armazon.id}' href="#" class="btn btn-primary subm1">Agregar al carrito</a>
+        </div>`;
+		document.getElementById("container-productos").append(card);
+		card.className = "card col-10 col-md-3 img-cat main-img";
+	}
 }
-armadoCard();
 
 //variable que selecciona todos los botones de las card
 const addButtons = document.querySelectorAll(`#btn-add`);
 
 //funcion, creo el evento del click del boton y llamo a la funcion traer la card seleccionada por el usuario
-addButtons.forEach(button =>{
-    button.addEventListener(`click`,addToCartClicked);
+addButtons.forEach((button) => {
+button.addEventListener(`click`, addToCartClicked);
 });
 
-// funcion que trae la card y extraigo los items de la card que me sirven. Llamo a otra funcion para armar el carrito
-//guarda en el localStorage el prod seleccionado para pasarlo luego a la hoja carrito
-function addToCartClicked(event){
-    const boton = event.target;
-    const traerCardEntera = boton.closest(`.card`);
-
-    const cardTitle = traerCardEntera.querySelector(`.card-title`).textContent;
-    const cardPrecio = traerCardEntera.querySelector(`.card-txt`).textContent;
-    const cardImg = traerCardEntera.querySelector(`.card-img-top`).src;
-
-    arrayProductos.push({cardTitle, cardPrecio, cardImg})
-    console.log(arrayProductos)
-
-    localStorage.setItem('lentes', JSON.stringify(arrayProductos))
-};
-
-
-//me llevo del array a la hoja carrito, el id y stock para restar en la compra
-datosDeStock=[];
-function restarAlStock(idProd, stockDispo){
-  datosDeStock.push({idProd,stockDispo});
-  console.log(datosDeStock);
-  localStorage.setItem(`stockId`, JSON.stringify(datosDeStock)); 
-  extraerStock(); 
-};
-
-//saco id y stock actualizado del localStorage, esto vino del carrito.js
-let actualizacionDelStock = JSON.parse(localStorage.getItem("stockActualizado"));
-const stockAct = [];
-function extraerStock(){
-        for (let item of actualizacionDelStock) {
-            let { idProd, cantidad } = item;
-            console.log(item);
-            almacenarArrayIdYStockAct(idProd, cantidad);
-            //break;
-        }
+//funcion que pushea al carrito el id del producto seleccionado a través del botón, después guarda en el
+//localStorage ese array para poder usarlo en carrito.js
+function addToCartClicked(event) {
+const boton = event.target;
+arrayProductos.push(boton.dataset.id);
+localStorage.setItem("lentes", JSON.stringify(arrayProductos));
 }
 
-//funcion para adherir al carrito lo sacado del localStorage que viene de la hoja carrito.js, cantidad a comprar y ID
-function almacenarArrayIdYStockAct(id, cantDeseada){
-    stockAct.push({id, cantDeseada});
-    console.log (stockAct);
-    restarAlStockLaCantVendida();
-}
+// // funcion que trae la card y extraigo los items de la card que me sirven. Llamo a otra funcion para armar el carrito
+// //guarda en el localStorage el prod seleccionado para pasarlo luego a la hoja carrito
+// function addToCartClicked(event){
+//     const boton = event.target;
+//     const traerCardEntera = boton.closest(`.card`);
 
-function restarAlStockLaCantVendida(){
-    for (let id of stockAct){
-        if (id == lentesDisponibles.id){
-            let {value} = cantidad 
-            let restarAlStock = lentesDisponibles.stock - value;
-            console.log(restarAlStock);
-            return restarAlStock; 
-        }
-    }
-}
+//     const cardTitle = traerCardEntera.querySelector(`.card-title`).textContent;
+//     const cardPrecio = traerCardEntera.querySelector(`.card-txt`).textContent;
+//     const cardImg = traerCardEntera.querySelector(`.card-img-top`).src;
 
-//funcion para actualizar el stock del armazón elegido
-function actualizarStockDelArray(){
-    for (let id of stockAct){
-        if (id == lentesDisponibles.id){
-            this.stock = stockDispo;
-        }
-    }
-}
+//     arrayProductos.push({cardTitle, cardPrecio, cardImg})
+//     console.log(arrayProductos)
+
+//     localStorage.setItem('lentes', JSON.stringify(arrayProductos))
+// };
+
+
+// //me llevo del array a la hoja carrito, el id y stock para restar en la compra
+// datosDeStock=[];
+// function restarAlStock(idProd, stockDispo){
+//   datosDeStock.push({idProd,stockDispo});
+//   console.log(datosDeStock);
+//   localStorage.setItem(`stockId`, JSON.stringify(datosDeStock)); 
+//   extraerStock(); 
+// };
+
+// //saco id y stock actualizado del localStorage, esto vino del carrito.js
+// let actualizacionDelStock = JSON.parse(localStorage.getItem("stockActualizado"));
+// const stockAct = [];
+// function extraerStock(){
+//         for (let item of actualizacionDelStock) {
+//             let { idProd, cantidad } = item;
+//             console.log(item);
+//             almacenarArrayIdYStockAct(idProd, cantidad);
+//             //break;
+//         }
+// }
+
+// //funcion para adherir al carrito lo sacado del localStorage que viene de la hoja carrito.js, cantidad a comprar y ID
+// function almacenarArrayIdYStockAct(id, cantDeseada){
+//     stockAct.push({id, cantDeseada});
+//     console.log (stockAct);
+//     restarAlStockLaCantVendida();
+// }
+
+// function restarAlStockLaCantVendida(){
+//     for (let id of stockAct){
+//         if (id == lentesDisponibles.id){
+//             let {value} = cantidad 
+//             let restarAlStock = Lentes.stock - value ;
+//             console.log(restarAlStock);
+//             return restarAlStock; 
+//         }
+//     }
+// }
+
+// //funcion para actualizar el stock del armazón elegido
+// function actualizarStockDelArray(){
+//     for (let id of stockAct){
+//         if (id == lentesDisponibles.id){
+//             this.stock = stockDispo;
+//         }
+//     }
+// }
